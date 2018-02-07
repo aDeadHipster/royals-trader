9a905c7b021a0a1a69127fbffa0409cb8aa45c9f
Justin Praas <jw.praas@gmail.com>
2018-02-07 19:58:39 +0100

    Added some stuff

--- profile.php
+++ profile.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <title>Royals Trader - Profile</title>
-    <?php include('dependencies.html')?>
+    <?php include('dependencies.html') ?>
 </head>
 <body>
 <div class="container-fluid">
@@ -24,6 +24,20 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
             <?php
             include("header.php");
             ?>
+
+            <!-- Profile Info Row -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="col-md-12 mt-2 p-3 border">
+                        <div class="col-md-2">
+                            <img src="https://lh3.googleusercontent.com/VT-PqxMMsA2wPy7kzmuKGDIzaA3AGuXKExqnfOfwTEy5AvLIMTranbfNGheRr457RD4=w300" class="img-profile">
+                        </div>
+                        <div class="col-md-10">
+                            <table></table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
